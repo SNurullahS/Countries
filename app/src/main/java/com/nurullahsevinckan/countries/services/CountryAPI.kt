bc11e37a7888d1e6998dbb9df34d99ff30f5e973
SNurullahS <nurullah.sevinckan91@gmail.com>
2024-10-09 23:58:43 +0300

    CountryAPI GET Api call implementation added

--- app/src/main/java/com/nurullahsevinckan/countries/services/CountryAPI.kt
+++ app/src/main/java/com/nurullahsevinckan/countries/services/CountryAPI.kt
@@ -1,4 +1,15 @@
 package com.nurullahsevinckan.countries.services
 
+import com.nurullahsevinckan.countries.model.Country
+import io.reactivex.Single
+import retrofit2.http.GET
+
 interface CountryAPI {
+    // API Link
+    // https://raw.githubusercontent.com/atilsamancioglu/IA19-DataSetCountries/master/countrydataset.json
+
+    @GET("atilsamancioglu/IA19-DataSetCountries/master/countrydataset.json")
+    //Single is just one time get data , we use it instead of using Observable method
+    fun getCountries():Single<List<Country>>
+
 }
